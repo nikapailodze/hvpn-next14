9f36fe5674f6c0f07c1982a0c058b038b5f25879
nikapailodze <nikapail.a01@gmail.com>
2024-12-20 20:10:36 +0400

    refeat: refactrired startbutton animation

--- app/Components/StartButton/StartButton.tsx
+++ app/Components/StartButton/StartButton.tsx
@@ -1,14 +1,26 @@
-import { IconEnum } from '@/global/Icon.enum';
-import styles from './StartButton.module.scss'
-import Image from 'next/image'
+import { IconEnum } from "@/global/Icon.enum";
+import styles from "./StartButton.module.scss";
+import Image from "next/image";
 
-const StartButton = ()=>{
-    return (
-        <div className={`${styles.conatiner} ${styles.circle} ${styles.pulse}`}>
-            <Image src={IconEnum.Play} width={108.401} height={108.401} alt='play'/>
-            
-        </div>
-    )
-}
+const StartButton = () => {
+  return (
+    // <div className={`${styles.conatiner} ${styles.circle} ${styles.pulse}`}>
+    //     <Image src={IconEnum.Play} width={108.401} height={108.401} alt='play'/>
+
+    // </div>
+    <div className={styles.main_container}>
+      <div className={styles.decor}>
+          <div className={styles.button_container}>
+            <Image
+              src={IconEnum.Play}
+              width={108.401}
+              height={108.401}
+              alt="play"
+            />
+          </div>
+      </div>
+    </div>
+  );
+};
 
 export default StartButton;
