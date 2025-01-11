3810105bc44cde910bcabd5aac6cfe00275a0d32
OtoQoqiashvili <167248626+OtoQoqiashvili@users.noreply.github.com>
2025-01-11 15:04:54 +0400

    timeDisplay

--- app/Components/StartButton/StartButton.tsx
+++ app/Components/StartButton/StartButton.tsx
@@ -1,19 +1,36 @@
 import { IconEnum } from "@/global/Icon.enum";
 import styles from "./StartButton.module.scss";
 import Image from "next/image";
+import { useState } from "react";
 
-const StartButton = () => {
+interface props {
+  isClicked: boolean;
+  timeDisplayFunc: () => void;
+}
+
+
+const StartButton = ({isClicked, timeDisplayFunc}: props) => {
+
+  const mainContainerClasses = `${styles.main_container} ${
+    isClicked ? styles.main_container_animated : ""
+  }`;
+  
+  const decorClasses = `${styles.decor} ${
+    isClicked ? styles.decor_animated : ""
+  }`;
   return (
-    <div className={styles.main_container}>
-      <div className={styles.decor}>
-          <div className={styles.button_container}>
-            <Image
-              src={IconEnum.Play}
-              width={108.401}
-              height={108.401}
-              alt="play"
-            />
-          </div>
+    <div className={mainContainerClasses}>
+      <div className={decorClasses}>
+        <div className={styles.button_container}
+          onClick={timeDisplayFunc}
+        >
+          <Image
+            src={IconEnum.Play}
+            width={108.401}
+            height={108.401}
+            alt="play"
+          />
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,27 @@ import { useRecoilState } from "recoil";
 import StartButton from "./Components/StartButton/StartButton";
 import styles from "./page.module.scss";
 import { locationIsSelectedState } from "@/global/atoms";
+import { useState } from "react";
 
 
 
 export default function Home() {
   const [states] = useRecoilState(locationIsSelectedState)
+  const [isOnClicked, setIsOnClicked] = useState(false)
+
+  function timeDisplay() {
+    setIsOnClicked(!isOnClicked)
+  }
 
   return (
     <div className={styles.page}>
-      <StartButton />
+      {isOnClicked ? 
+      <div>
+        <p></p>
+        <p></p>
+      </div>
+       : ''}
+      <StartButton isClicked={isOnClicked} timeDisplayFunc={timeDisplay} />
     </div>
   );
 }
